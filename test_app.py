0855af33b3a1801f6cd2637dd7ddb19799941278
KaiJones7 <kai@eebz.com>
2023-03-14 07:32:00 +0000

    ticket 3 ignore empyty lines

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from extract import get_csv
+from extract import get_csv, remove_duplicates, ignore_empty_lines
 
 def test_input_is_list():
     #Arrange
@@ -13,7 +13,7 @@ def test_input_is_list():
 
 
 def test_input_is_correct():
-    #Arrange]
+    #Arrange
     filename = "results.csv"
     expected_columns = ["user_id","first_name","last_name","answer_1","answer_2","answer_3"]
     expected_row_count = 25
@@ -30,3 +30,32 @@ def test_input_is_correct():
 
 
 
+def test_remove_duplicate():
+    data = [
+        ['1', 'John', 'Doe', 'Yes', 'No', '5'],
+        ['2', 'Jane', 'Doe', 'No', 'Yes', '8'],
+        ['1', 'John', 'Doe', 'Yes', 'No', '5'],
+        ['4', 'Bob', 'Smith', 'Yes', 'Yes', '9']
+    ]
+    expected_data = [
+        ['1', 'John', 'Doe', 'Yes', 'No', '5'],
+        ['2', 'Jane', 'Doe', 'No', 'Yes', '8'],
+        ['4', 'Bob', 'Smith', 'Yes', 'Yes', '9']
+    ]
+    assert remove_duplicates(data) == expected_data
+
+
+def test_ignore_empty_lines():
+    data = [
+        ['1', 'John', 'Doe', 'Yes', 'No', '5'],
+        ['', '', '', '', '', ''],
+        ['2', 'Jane', 'Doe', 'No', 'Yes', '8'],
+        ['3', 'Bob', 'Smith', 'Yes', 'Yes', '9'],
+        ['', '', '', '', '', '']
+    ]
+    expected_data = [
+        ['1', 'John', 'Doe', 'Yes', 'No', '5'],
+        ['2', 'Jane', 'Doe', 'No', 'Yes', '8'],
+        ['3', 'Bob', 'Smith', 'Yes', 'Yes', '9']
+    ]
+    assert ignore_empty_lines(data) == expected_data
