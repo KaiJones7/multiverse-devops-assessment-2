a986b7a4430f6ce1e9f46400d07464c19d8a8b3f
KaiJones7 <kai@eebz.com>
2023-03-15 18:32:48 +0000

    updated terraform and workflow

--- terraform-iac/terraform.tf
+++ terraform-iac/terraform.tf
@@ -1,17 +1,2 @@
 provider "aws" {
-    region = "eu-west-2"
-}
-
-resource "aws_s3_bucket" "this" {
-    bucket_prefix = "app-kai-jones"
-    force_destroy = true
-    tags = {
-        Name = "Survey App Bucket"
-    }
-}
-
-resource "aws_s3_object" "test_file" {
-    bucket = aws_s3_bucket.this.id
-    key = "test_file.txt"
-    content = "hello world!!"
-}
+    region = "eu-west-2"}
