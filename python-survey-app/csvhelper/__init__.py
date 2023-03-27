b79c8ec5f88b762c645c98bf1ad7f2752954a518
KaiJones7 <kai@eebz.com>
2023-03-27 19:09:50 +0100

    updated print_clean_data

--- python-survey-app/csvhelper/__init__.py
+++ python-survey-app/csvhelper/__init__.py
@@ -60,9 +60,7 @@ def write_clean_data_to_file(input_file, output_file):
 
 
 def print_clean_data(file):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(dir_path, file)
-    with open(file_path) as f:
+    with open(file) as f:
         reader = csv.DictReader(f)
         print(f"{'user_id':<10} {'First Name':<20} {'Last Name':<20} {'Answer 1':<15} {'Answer 2':<15} {'Answer 3':<15}")
         for row in reader:
